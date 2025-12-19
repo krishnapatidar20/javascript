f22fdfc180ffd985c83fd612350267ec4e4c8467
krishnapatidar20 <krishnapatidaraug20@gmail.com>
2025-12-19 16:30:50 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,29 @@
+/*
+ Primitive type
+ 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+ Reference type (non-primitive)
+ Array, Objects, Functions 
+
+*/ 
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+//const bigNumber = 34343434343435656354
+
+
+// ***************Memories************
+
+// Stack(Primitive) , Heap(Non-primitive)
+
+                    
