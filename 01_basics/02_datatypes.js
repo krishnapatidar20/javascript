5610c1b8b3b136a86123c930f5d903dfd8406569
krishnapatidar20 <krishnapatidaraug20@gmail.com>
2025-12-19 11:21:20 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+// alert( 3 + 4 ) // we are usoingnode.js, not browser
+
+let name = "krishna"
+let age = 19
+let isLoggedIn = false
+let state;
+
+// number => 2 to the power 53 (range)
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
+
+
+
