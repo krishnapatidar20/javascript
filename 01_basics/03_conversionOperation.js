24fa8d2534f2561f1cdaca5b7066baea9466cff2
krishnapatidar20 <krishnapatidaraug20@gmail.com>
2025-12-19 16:30:11 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = true
+ let score = true
 
 console.log(typeof score);
 console.log(typeof(score));
@@ -25,6 +25,41 @@ let someNumber = 44
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+
+// ******************Operations******************
+
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+
+let str1 = "hello"
+let str2 = " krishna"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" +2+2);
+console.log(1+2+"2");
+
+console.log(+true);
+console.log(+"");
+
+let num1 , num2 , num3
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
 
 
 
