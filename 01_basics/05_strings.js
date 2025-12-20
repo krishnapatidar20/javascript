71d07aadb2a489e9257a2e1afceb6d2cc68c384c
krishnapatidar20 <krishnapatidaraug20@gmail.com>
2025-12-20 05:44:09 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,25 @@
+const name = "krishna"
+const repoCount = 1
+
+// console.log(name + repoCount + " Value"); //outdated method
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('kp-20')
+/*
+console.log(gameName[1]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('p'));
+*/
+
+const newString = gameName.substring(0,3)
+console.log(newString);
+
+const oneString = "    krishna    "
+console.log(oneString);
+console.log(oneString.trim());
+
