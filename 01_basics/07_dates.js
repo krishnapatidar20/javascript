e0f273cdf28af9e9b96be34b2ef145ddfb82d1e0
krishnapatidar20 <krishnapatidaraug20@gmail.com>
2025-12-20 11:19:44 +0000

    date and time

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,8 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+
+let myCreatedDate = new Date(2025, 0, 12)
+console.log(myCreatedDate.toDateString());
