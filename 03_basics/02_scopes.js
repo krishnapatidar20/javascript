778f2118c02fb31dc2608949347c7cbf087d2d18
krishnapatidar20 <krishnapatidaraug20@gmail.com>
2025-12-22 12:22:47 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -7,12 +7,61 @@ if(true){
     let a = 10
     const b = 20
     var c = 30
-    console.log("inner: ", a);
+    //console.log("inner: ", a);
 
 }
 
                
 
-console.log(a);
+//console.log(a);
 //console.log(b);
 //console.log(c);
+
+
+
+function one(){
+    const username = "krishna"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    //console.log(website);
+
+    two()
+}
+
+//one()
+
+
+if (true) {
+    const username = "krishna"
+
+    if(username === "krishna"){
+        const website = " youtube"
+        //console.log(username + website);
+        
+    }
+    //console.log(website);
+    
+}
+
+//console.log(username);
+
+
+
+// +++++++++++++++ intresting ++++++++++++++
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+
+
+
+const addtwo = function(num){
+    return num + 2
+}
+console.log(addtwo(5))
+
