6d9fa24126f8d2930d61c487b85b2678b5d2b87e
krishnapatidar20 <krishnapatidaraug20@gmail.com>
2025-12-24 15:44:13 +0000

    for each

---
+++ 05_iterations/five.js
@@ -0,0 +1,32 @@
+//const coding = ["js", "ruby", "java", "c++"]
+
+//coding.forEach( function (item) {
+    //console.log(item);
+    
+//} )
+//coding.forEach(  (item) => {
+    //console.log(item);
+    
+//} )
+
+
+
+const coding = [
+    {
+        langName: "javascript",
+        filNname: "js"
+    },
+    {
+        langName: "java",
+        filNname: "java"
+    },
+    {
+        langName: "python",
+        filNname: "py"
+    }
+]
+
+coding.forEach((item) => {
+    console.log(item.langName);
+    
+})
