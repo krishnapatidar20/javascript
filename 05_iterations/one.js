732e29a6bb040c602c401d0f47082a8b10ffd1c1
krishnapatidar20 <krishnapatidaraug20@gmail.com>
2025-12-24 12:36:39 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,54 @@
+// for loop
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if (element == 5) {
+        //console.log("5 is best number");
+        
+    }
+    //console.log(element);
+    
+}
+
+
+for (let i = 1; i <= 10; i++) {
+    //console.log(`Outer loop : ${i}`);
+    
+    for (let j = 1; j <= 5; j++) {
+        //console.log(`Inner loop ${j} and inner loop ${i}`);
+        //console.log(i + '*' + j + '=' + i*j);
+        
+        
+    }
+    
+}
+
+
+let myArr = ["flash", "batman", "ironman"] 
+//console.log(myArr.length);
+
+for (let index = 0; index < myArr.length; index++) { // < because 0 based indexing
+    const element = myArr[index];
+    //console.log(element);
+    
+}
+
+
+// break and continue
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        //console.log(`Dected 5`);
+        break
+    }
+    //console.log(`Value if i is ${index}`); 
+}
+
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`Dected 5`);
+        continue
+    }
+    console.log(`Value if i is ${index}`); 
+}
