4e0a7e78c7b3b5530edfa9e72cf810e1bb00bf68
krishnapatidar20 <krishnapatidaraug20@gmail.com>
2025-12-24 15:43:41 +0000

    for of

---
+++ 05_iterations/three.js
@@ -0,0 +1,27 @@
+// for of loop
+
+const arr = [1,2,3,4,5]
+
+for (const num of arr) {
+   // console.log(num);
+    
+}
+
+const greetings = "hello world"
+for (const greet of greetings) {
+    //console.log(`each char is ${greet}` );
+    
+}
+
+// Maps 
+
+const map = new Map()
+map.set('IN',"India")
+map.set('UK',"United Kingdom")
+map.set('FR',"France")
+//console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+    
+}
