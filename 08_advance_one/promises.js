a4bc454d59313c9003e971fb82baf049be7d16b1
krishnapatidar20 <krishnapatidaraug20@gmail.com>
2025-12-31 07:36:52 +0000

    promises

--- 08_advance_one/promises.js
+++ 08_advance_one/promises.js
@@ -2,7 +2,7 @@ const promiseOne = new Promise(function(resolve, reject){
     //do an async task
     //DB calls, cryptography, network
     setTimeout(function(){
-        //console.log('Async tak is complete');
+        //console.log('Async task is complete');
         resolve()
     },1000)
 })
@@ -74,7 +74,7 @@ setTimeout(function () {
         } 
     },1000)
 })
-
+/*
 async function consumePromiseFive(){
     const response = await promiseFive
     console.log(response);
@@ -82,3 +82,44 @@ async function consumePromiseFive(){
 }
 
 consumePromiseFive()
+*/
+
+
+
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+        
+    }
+    
+}
+
+consumePromiseFive()
+
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ", error);
+//     }
+// }
+
+//getAllUsers()
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
+
+
